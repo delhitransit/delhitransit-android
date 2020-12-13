330b0333a7dcab5038fa05f56777e93b1a1954e3
Tanmay Singal <tanmaysingal@icloud.com>
2020-12-13 14:11:03 +0530

    Navigate to FavouriteStopsFragment from BottomNav

--- app/src/main/java/com/delhitransit/delhitransit_android/fragment/FavouriteStopsViewModel.java
+++ app/src/main/java/com/delhitransit/delhitransit_android/fragment/FavouriteStopsViewModel.java
@@ -2,19 +2,20 @@ package com.delhitransit.delhitransit_android.fragment;
 
 import android.app.Application;
 
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.delhitransit.delhitransit_android.database.FavouriteStopsRepository;
 import com.delhitransit.delhitransit_android.pojos.stops.StopsResponseData;
 
 import java.util.List;
 
-public class FavouriteStopsViewModel extends ViewModel {
+public class FavouriteStopsViewModel extends AndroidViewModel {
     private final LiveData<List<StopsResponseData>> mAllFavouriteStops;
     private final FavouriteStopsRepository mRepository;
 
     public FavouriteStopsViewModel(Application application) {
+        super(application);
         mRepository = new FavouriteStopsRepository(application);
         mAllFavouriteStops = mRepository.getAllFavouriteStops();
     }
