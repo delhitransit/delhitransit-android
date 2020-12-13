0a4f8a2f74d898232fb1b4888ecb3b69e9bc932e
Tanmay Singal <tanmaysingal@icloud.com>
2020-12-13 10:51:32 +0530

    Fixed background color of settings fragment

--- app/src/main/java/com/delhitransit/delhitransit_android/fragment/FullScreenFragment.java
+++ app/src/main/java/com/delhitransit/delhitransit_android/fragment/FullScreenFragment.java
@@ -19,20 +19,18 @@ public class FullScreenFragment extends Fragment {
      */
     private static final int UI_ANIMATION_DELAY = 300;
 
+    // Note that some of these constants are new as of API 16 (Jelly Bean)
+    // and API 19 (KitKat). It is safe to use them, as they are inlined
+    // at compile-time and do nothing on earlier devices.
+    private static final int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
     private final Handler mHideHandler = new Handler();
-    private final Runnable mHidePart2Runnable = () -> {
-        // Delayed removal of status and navigation bar
-
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-
+    private final Runnable mHideRunnable = () -> {
         Activity activity = getActivity();
         if (activity != null
                 && activity.getWindow() != null) {
@@ -44,7 +42,7 @@ public class FullScreenFragment extends Fragment {
         }
 
     };
-    private final Runnable mShowPart2Runnable = () -> {
+    private final Runnable mShowRunnable = () -> {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.show();
@@ -67,7 +65,7 @@ public class FullScreenFragment extends Fragment {
     public void onPause() {
         super.onPause();
         if (getActivity() != null && getActivity().getWindow() != null) {
-            getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            getActivity().getWindow().clearFlags(flags);
 
             // Clear the systemUiVisibility flag
             getActivity().getWindow().getDecorView().setSystemUiVisibility(0);
@@ -83,15 +81,15 @@ public class FullScreenFragment extends Fragment {
         }
 
         // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+        mHideHandler.removeCallbacks(mShowRunnable);
+        mHideHandler.postDelayed(mHideRunnable, UI_ANIMATION_DELAY);
     }
 
     @SuppressLint("InlinedApi")
     private void show() {
         // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mShowRunnable, UI_ANIMATION_DELAY);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.show();
