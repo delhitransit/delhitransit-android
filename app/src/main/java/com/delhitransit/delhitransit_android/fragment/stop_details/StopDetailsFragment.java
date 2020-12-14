96d2ee65312f71b22648c0f317767efae5ecbf53
singaltanmay <tanmaysingal@icloud.com>
2020-12-14 17:47:10 +0530

    Added Extended FAB
    Signed-off-by: Tanmay Singal <tanmaysingal@icloud.com>

--- app/src/main/java/com/delhitransit/delhitransit_android/fragment/stop_details/StopDetailsFragment.java
+++ app/src/main/java/com/delhitransit/delhitransit_android/fragment/stop_details/StopDetailsFragment.java
@@ -1,25 +1,25 @@
 package com.delhitransit.delhitransit_android.fragment.stop_details;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.delhitransit.delhitransit_android.R;
+import com.delhitransit.delhitransit_android.pojos.stops.StopDetail;
 
 public class StopDetailsFragment extends Fragment {
 
+    private final StopDetail stop;
     private StopDetailsViewModel mViewModel;
 
-    public static StopDetailsFragment newInstance() {
-        return new StopDetailsFragment();
+    public StopDetailsFragment(StopDetail stop) {
+        this.stop = stop;
     }
 
     @Override
