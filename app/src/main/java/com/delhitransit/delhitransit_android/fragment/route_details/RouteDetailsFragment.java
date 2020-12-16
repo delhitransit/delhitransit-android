fe94bc6ca230e905bcf1c1898abed785c866ffb8
singaltanmay <tanmaysingal@icloud.com>
2020-12-16 15:31:18 +0530

    Created and designed RouteDetailsFragment
    Signed-off-by: Tanmay Singal <tanmaysingal@icloud.com>
    
    Created and designed RouteDetailsFragment
    
    Signed-off-by: Tanmay Singal <tanmaysingal@icloud.com>

---
+++ app/src/main/java/com/delhitransit/delhitransit_android/fragment/route_details/RouteDetailsFragment.java
@@ -0,0 +1,32 @@
+package com.delhitransit.delhitransit_android.fragment.route_details;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.delhitransit.delhitransit_android.R;
+
+public class RouteDetailsFragment extends Fragment {
+
+    private RouteDetailsViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_route_details, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RouteDetailsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/delhitransit/delhitransit_android/fragment/route_details/RouteDetailsViewModel.java
@@ -0,0 +1,7 @@
+package com.delhitransit.delhitransit_android.fragment.route_details;
+
+import androidx.lifecycle.ViewModel;
+
+public class RouteDetailsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
